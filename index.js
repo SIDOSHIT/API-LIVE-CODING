54ae6dadb8321a16b4ade99e7f75c629cedaddb5
SIDOSHIT <danil.gromov00@inbox.ru>
2023-05-10 23:04:22 +0300

    Выносит форму входа вотдеьный компонент

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // 2. Реализовать форму регистрации
 
 import { addTodo, deleteTodo, getTodos } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
 
 // TODO: Получать из хранилища данных
 let tasks = [];
@@ -24,28 +25,9 @@ const fetchTodosAndRender = () => {
 const renderApp = () => {
   const appEl = document.getElementById("app");
   if (!token) {
-    const appHtml = `
-                <h1>Список задач</h1>
-                <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                    Логин
-                    <input type="text" id="login-input" class="input" />
-                    <br />
-                    Пароль
-                    <input type="text" id="login-input" class="input" />
-                </div>
-                <br />
-                <button class="button" id="login-button">Войти</button>
-                </div>`;
-
-    appEl.innerHTML = appHtml;
-
-    document.getElementById("login-button").addEventListener("click", () => {
-      token =
-        "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-      fetchTodosAndRender();
-    });
+    renderLoginComponent({appEl, setToken: (newtoken) => {
+        token  = newtoken;
+    }, fetchTodosAndRender});
 
     return;
   }
@@ -138,6 +120,4 @@ const renderApp = () => {
       });
   });
 };
-
-// fetchTodosAndRender();
 renderApp();
